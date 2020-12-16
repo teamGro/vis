0f7ac0237249775810d46bb66ff27490197e882f
timas <vas.liwanov@yandex.ru>
2020-12-16 20:42:10 +0300

    settings

---
+++ public/scripts/main.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    function hello() {
+        console.log('hello');
+    }
+
+    hello();
+
+}());
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7OyJ9
