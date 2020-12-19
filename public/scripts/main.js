e0f3515c94543e396f4c4f2ce45b3a4a1f5adb99
timas <vas.liwanov@yandex.ru>
2020-12-19 17:09:03 +0300

    added questions

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -104,6 +104,36 @@
         headerSlider.mount();
     }, 500);
 
+    const questionsList = $('.questions__list');
+    let activeQuestion = null;
+    questionsList.on('click', function (e) {
+        let $target = $(e.target);
+        if (!$target.closest('.questions__wrap')) return;
+
+        $target = $target.closest('.questions__wrap');
+        let $parent = $target.parent();
+
+
+        if (activeQuestion && activeQuestion.attr('data-num') == $parent.attr('data-num')) {
+            activeQuestion.find('.questions__describe').slideUp('slow');
+            $target.removeClass('questions__wrap_open');
+            activeQuestion = null;
+            return;
+        }
+
+        if (activeQuestion) {
+            activeQuestion.find('.questions__describe').slideUp('slow');
+            activeQuestion.find('.questions__wrap_open').removeClass('questions__wrap_open');
+        }
+
+        setTimeout(() => {
+            $target.addClass('questions__wrap_open');
+            $parent.find('.questions__describe').slideDown('slow');
+            activeQuestion = $parent;
+
+        }, 300);
+    });
+
 }());
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OzsifQ==
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OzsifQ==
