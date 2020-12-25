f95ffa7a20526fa280e2e5da70ff0db52cf36412
Tima <bredos11@yandex.ru>
2020-12-25 13:30:29 +0300

    added anime and render

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -146,43 +146,158 @@
     }
 
     const tabs = $('.tabs');
-    let data = {
-        delivery: [
-            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a posuere mi. Morbi imperdiet felis id vulputate rhoncus. Curabitur pellentesque cursus gravida. Curabitur ornare dignissim mauris at hendrerit. Duis ut cursus arcu. Duis laoreet dui ac ultrices congue. Suspendisse lacus lacus, vulputate a eleifend ac, gravida nec tortor. Morbi at sagittis tortor. Sed egestas cursus accumsan. In et metus luctus, accumsan leo vitae, tincidunt sapien.`,
-            `Donec at orci a risus aliquet aliquet. Aliquam rutrum est sit amet eleifend porta. Sed in erat metus. Nam tristique volutpat purus eget ornare. Phasellus ultrices turpis nec condimentum dapibus. Suspendisse id est at sem efficitur placerat ut vitae leo. Aenean consectetur pulvinar mollis. Sed ac sodales orci. Ut scelerisque nulla sit amet consectetur commodo. Vivamus nec velit ut eros dignissim consequat. Suspendisse elementum dolor sed purus rhoncus mollis. Nullam laoreet ligula urna, non ultricies tortor posuere at. Pellentesque aliquet nunc nec lorem commodo porttitor et nec ipsum. Sed eget diam sodales, rutrum lorem eu, molestie erat.`,
-            `Suspendisse pellentesque felis vitae sapien interdum, ut tincidunt est porttitor. Nam purus leo, vulputate eu tincidunt vel, placerat in eros. Nam et lobortis arcu. Nulla augue urna, placerat in felis nec, tempor mollis tellus. Sed hendrerit diam ligula, ac molestie diam euismod eu. Nulla sem justo, faucibus in sagittis ut, maximus eu lorem. Ut ut tortor hendrerit, tempus neque vitae, pharetra tellus. Duis mattis mauris at justo cursus tristique. Mauris faucibus luctus sem. Donec ante ante, pretium vel varius non, pellentesque id eros. Nunc faucibus pharetra lacus in accumsan. Nunc imperdiet viverra enim quis venenatis. Praesent non sem ac leo molestie euismod. Suspendisse mattis dui a risus egestas eleifend.`],
-        rent: [
-            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a posuere mi. Morbi imperdiet felis id vulputate rhoncus. Curabitur pellentesque cursus gravida. Curabitur ornare dignissim mauris at hendrerit. Duis ut cursus arcu. Duis laoreet dui ac ultrices congue. Suspendisse lacus lacus, vulputate a eleifend ac, gravida nec tortor. Morbi at sagittis tortor. Sed egestas cursus accumsan. In et metus luctus, accumsan leo vitae, tincidunt sapien.`,
-            `Donec at orci a risus aliquet aliquet. Aliquam rutrum est sit amet eleifend porta. Sed in erat metus. Nam tristique volutpat purus eget ornare. Phasellus ultrices turpis nec condimentum dapibus. Suspendisse id est at sem efficitur placerat ut vitae leo. Aenean consectetur pulvinar mollis. Sed ac sodales orci. Ut scelerisque nulla sit amet consectetur commodo. Vivamus nec velit ut eros dignissim consequat. Suspendisse elementum dolor sed purus rhoncus mollis. Nullam laoreet ligula urna, non ultricies tortor posuere at. Pellentesque aliquet nunc nec lorem commodo porttitor et nec ipsum. Sed eget diam sodales, rutrum lorem eu, molestie erat.`,
-            `Suspendisse pellentesque felis vitae sapien interdum, ut tincidunt est porttitor. Nam purus leo, vulputate eu tincidunt vel, placerat in eros. Nam et lobortis arcu. Nulla augue urna, placerat in felis nec, tempor mollis tellus. Sed hendrerit diam ligula, ac molestie diam euismod eu. Nulla sem justo, faucibus in sagittis ut, maximus eu lorem. Ut ut tortor hendrerit, tempus neque vitae, pharetra tellus. Duis mattis mauris at justo cursus tristique. Mauris faucibus luctus sem. Donec ante ante, pretium vel varius non, pellentesque id eros. Nunc faucibus pharetra lacus in accumsan. Nunc imperdiet viverra enim quis venenatis. Praesent non sem ac leo molestie euismod. Suspendisse mattis dui a risus egestas eleifend.`]
+
+    let markup = {
+      delivery: {
+        markup: function (data) {
+          return `<p class="price__delivery">${data}</p>`;
+        },
+        data: [
+          `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a posuere mi. Morbi imperdiet felis id vulputate rhoncus. Curabitur pellentesque cursus gravida. Curabitur ornare dignissim mauris at hendrerit. Duis ut cursus arcu. Duis laoreet dui ac ultrices congue. Suspendisse lacus lacus, vulputate a eleifend ac, gravida nec tortor. Morbi at sagittis tortor. Sed egestas cursus accumsan. In et metus luctus, accumsan leo vitae, tincidunt sapien.`,
+          `Donec at orci a risus aliquet aliquet. Aliquam rutrum est sit amet eleifend porta. Sed in erat metus. Nam tristique volutpat purus eget ornare. Phasellus ultrices turpis nec condimentum dapibus. Suspendisse id est at sem efficitur placerat ut vitae leo. Aenean consectetur pulvinar mollis. Sed ac sodales orci. Ut scelerisque nulla sit amet consectetur commodo. Vivamus nec velit ut eros dignissim consequat. Suspendisse elementum dolor sed purus rhoncus mollis. Nullam laoreet ligula urna, non ultricies tortor posuere at. Pellentesque aliquet nunc nec lorem commodo porttitor et nec ipsum. Sed eget diam sodales, rutrum lorem eu, molestie erat.`,
+          `Suspendisse pellentesque felis vitae sapien interdum, ut tincidunt est porttitor. Nam purus leo, vulputate eu tincidunt vel, placerat in eros. Nam et lobortis arcu. Nulla augue urna, placerat in felis nec, tempor mollis tellus. Sed hendrerit diam ligula, ac molestie diam euismod eu. Nulla sem justo, faucibus in sagittis ut, maximus eu lorem. Ut ut tortor hendrerit, tempus neque vitae, pharetra tellus. Duis mattis mauris at justo cursus tristique. Mauris faucibus luctus sem. Donec ante ante, pretium vel varius non, pellentesque id eros. Nunc faucibus pharetra lacus in accumsan. Nunc imperdiet viverra enim quis venenatis. Praesent non sem ac leo molestie euismod. Suspendisse mattis dui a risus egestas eleifend.`,
+        ],
+      },
+      rent: {
+        markup: function (data) {
+          return `<p class="price__delivery">${data}</p>`;
+        },
+        data: [
+          `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a posuere mi. Morbi imperdiet felis id vulputate rhoncus. Curabitur pellentesque cursus gravida. Curabitur ornare dignissim mauris at hendrerit. Duis ut cursus arcu. Duis laoreet dui ac ultrices congue. Suspendisse lacus lacus, vulputate a eleifend ac, gravida nec tortor. Morbi at sagittis tortor. Sed egestas cursus accumsan. In et metus luctus, accumsan leo vitae, tincidunt sapien.`,
+          `Donec at orci a risus aliquet aliquet. Aliquam rutrum est sit amet eleifend porta. Sed in erat metus. Nam tristique volutpat purus eget ornare. Phasellus ultrices turpis nec condimentum dapibus. Suspendisse id est at sem efficitur placerat ut vitae leo. Aenean consectetur pulvinar mollis. Sed ac sodales orci. Ut scelerisque nulla sit amet consectetur commodo. Vivamus nec velit ut eros dignissim consequat. Suspendisse elementum dolor sed purus rhoncus mollis. Nullam laoreet ligula urna, non ultricies tortor posuere at. Pellentesque aliquet nunc nec lorem commodo porttitor et nec ipsum. Sed eget diam sodales, rutrum lorem eu, molestie erat.`,
+          `Suspendisse pellentesque felis vitae sapien interdum, ut tincidunt est porttitor. Nam purus leo, vulputate eu tincidunt vel, placerat in eros. Nam et lobortis arcu. Nulla augue urna, placerat in felis nec, tempor mollis tellus. Sed hendrerit diam ligula, ac molestie diam euismod eu. Nulla sem justo, faucibus in sagittis ut, maximus eu lorem. Ut ut tortor hendrerit, tempus neque vitae, pharetra tellus. Duis mattis mauris at justo cursus tristique. Mauris faucibus luctus sem. Donec ante ante, pretium vel varius non, pellentesque id eros. Nunc faucibus pharetra lacus in accumsan. Nunc imperdiet viverra enim quis venenatis. Praesent non sem ac leo molestie euismod. Suspendisse mattis dui a risus egestas eleifend.`,
+        ],
+      },
+      grout: {
+        markupHeader: `<li class="price__item price__item_head">
+        <div class="price__item-wrap">
+            <span class="price__name price__name_head">Наименование</span>
+            <span class="price__price price__price_head">Цена</span>
+        </div>
+    </li>`,
+        markup: `<li class="price__item">
+        <div class="price__img-wrap">
+            <img src="./img/sales.png" alt="" class="price__imgprice__sales">
+            <img src="./img/hit.png" alt="" class="price__img price__hit">
+        </div>
+        <div class="price__item-wrap">
+            <span class="price__name">Бетон В 22.5 М 300</span>
+            <span class="price__price">3 450 руб / м3 </span>
+        </div>
+        <button type="button" class="price__btn" data-type="callback">
+            <span>Подробнее</span>
+            <img src="./img/arrow-btn-header.png" alt="" />
+        </button>
+    </li>`,
+        data: [
+          { name: 'Бетон В 22.5 М 300', price: '3 450 руб / м3' },
+          { name: 'Бетон В 22.5 М 300', price: '3 450 руб / м3' },
+          { name: 'Бетон В 22.5 М 300', price: '3 450 руб / м3' },
+        ],
+      },
+      concrete: {
+        markupHeader: `<li class="price__item price__item_head">
+        <div class="price__item-wrap">
+            <span class="price__name price__name_head">Наименование</span>
+            <span class="price__price price__price_head">Цена</span>
+        </div>
+    </li>`,
+        markup: function (data) {
+          return `<li class="price__item">
+        <div class="price__img-wrap">
+            <img src="./img/sales.png" alt="" class="price__imgprice__sales">
+            <img src="./img/hit.png" alt="" class="price__img price__hit">
+        </div>
+        <div class="price__item-wrap">
+            <span class="price__name">${data.name}</span>
+            <span class="price__price">${data.price}</span>
+        </div>
+        <button type="button" class="price__btn" data-type="callback">
+            <span>Подробнее</span>
+            <img src="./img/arrow-btn-header.png" alt="" />
+        </button>
+    </li>`;
+        },
+        data: [
+          { name: 'Бетон В 22.5 М 300', price: '3 450 руб / м3' },
+          { name: 'Бетон В 22.5 М 300', price: '3 450 руб / м3' },
+          { name: 'Бетон В 22.5 М 300', price: '3 450 руб / м3' },
+        ],
+      },
+      grout: {
+        markupHeader: `<li class="price__item price__item_head">
+        <div class="price__item-wrap">
+            <span class="price__name price__name_head">Наименование</span>
+            <span class="price__price price__price_head">Цена</span>
+        </div>
+    </li>`,
+        markup: function (data) {
+          return `<li class="price__item">
+        <div class="price__img-wrap">
+            <img src="./img/sales.png" alt="" class="price__imgprice__sales">
+            <img src="./img/hit.png" alt="" class="price__img price__hit">
+        </div>
+        <div class="price__item-wrap">
+            <span class="price__name">${data.name}</span>
+            <span class="price__price">${data.price}</span>
+        </div>
+        <button type="button" class="price__btn" data-type="callback">
+            <span>Подробнее</span>
+            <img src="./img/arrow-btn-header.png" alt="" />
+        </button>
+    </li>`;
+        },
+        data: [
+          { name: 'Бетон В 22.5 М 300', price: '3 450 руб / м3' },
+          { name: 'Бетон В 22.5 М 300', price: '3 450 руб / м3' },
+          { name: 'Бетон В 22.5 М 300', price: '3 450 руб / м3' },
+        ],
+      },
     };
     function setActiveTabAndShowContent() {
-        tabs.on('click', function (e) {
-            let $target = $(e.target);
-            if ($target.prop('tagName') != 'LI') return;
+      tabs.on('click', function (e) {
+        let $target = $(e.target);
+        if ($target.prop('tagName') != 'LI') return;
 
-            setActiveTab($target, $(this));
-            showContent($target);
-        });
+        setActiveTab($target, $(this));
+        showContent($target);
+      });
     }
 
     function setActiveTab(target, parent) {
-        parent.find('.tabs__item_active').removeClass('tabs__item_active');
-        target.addClass('tabs__item_active');
+      parent.find('.tabs__item_active').removeClass('tabs__item_active');
+      target.addClass('tabs__item_active');
     }
 
-    function crateMarkup(data, parent, getMarkup) {
+    function createMarkup(data, parent) {
+      parent.find('.price__item').each(function (i) {
+        $(this).addClass('price__item_inactive');
+      });
+
+      parent.find('.price__delivery').each(function (i) {
+        $(this).addClass('price__delivery_inactive');
+      });
+
+      setTimeout(() => {
         parent.empty();
-        data.forEach(item => {
-            parent.append(`<p class="price__delivery">${item}</p>`);
+        if (data.markupHeader) {
+          parent.append(data.markupHeader);
+          data.data.forEach((item) => {
+            parent.append(data.markup(item));
+          });
+
+          return;
+        }
+
+        data.data.forEach((item) => {
+          parent.append(data.markup(item));
         });
+      }, 500);
     }
-
-
 
     function showContent(target) {
-        let attr = target.attr('data-tab');
-        crateMarkup(data[attr], $('.price__list'));
+      let attr = target.attr('data-tab');
+      createMarkup(markup[attr], $('.price__list'));
     }
 
     const btnBurger$1 = $('.burger');
@@ -469,4 +584,4 @@
 
 }());
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OzsifQ==
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7In0=
